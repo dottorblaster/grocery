7e1e2af9d7b78258d5cef96da658980fea2440b4
Alessio Biancalana <dottorblaster@gmail.com>
2016-08-23 14:08:35 +0200

    error_handler implementation

---
+++ error_handler.c
@@ -0,0 +1,15 @@
+#include "error_handler.h"
+
+void handle_error(int type, int socket_fd) {
+  switch(type) {
+		case ERROR:
+			write(socket_fd, "HTTP/1.1 403 Forbidden\nContent-Length: 185\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>Internal Server Error</h1>\nOperation gone bad. Sorry.\n</body></html>\n", 215);
+			break;
+		case NOTFOUND:
+			write(socket_fd, "HTTP/1.1 404 Not Found\nContent-Length: 136\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\nThe requested URL was not found on this server.\n</body></html>\n", 224);
+			break;
+		case FORBIDDEN:
+			write(socket_fd, "HTTP/1.1 403 Forbidden\nContent-Length: 185\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>Forbidden</h1>\nThe requested URL, file type or operation is not allowed on this simple static file webserver.\n</body></html>\n", 271);
+			break;
+	}
+}
---
+++ error_handler.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "globals.h"
