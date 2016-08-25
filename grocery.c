be24b2aca1506a095730a4219e361d92e68facd6
Alessio Biancalana <dottorblaster@gmail.com>
2016-08-25 17:09:06 +0200

    first server implementation

--- grocery.c
+++ grocery.c
@@ -1,5 +1,52 @@
 #include "grocery.h"
 
+void print_help() {
+	printf("This is a simple help string");
+	exit(0);
+}
+
+void preliminary_checks(int argc, char **argv) {
+	if (argc != 2 || !strcmp(argv[1], "-h")) {
+		print_help();
+	}
+	if (atoi(argv[1]) < 0 || atoi(argv[1]) > 60000) {
+		logger(ERROR, "Invalid port number", argv[1]);
+	}
+}
+
+void spawn_server(char **argv) {
+	int i, pid, port, lfd, sock_fd;
+	static struct sockaddr_in cli;
+	static struct sockaddr_in server;
+
+	socklen_t l;
+
+	/* Just ignore SIGCHLD and SIGHUP, we don't care. */
+	signal(SIGHUP, SIG_IGN);
+	signal(SIGCHLD, SIG_IGN);
+
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_port = htons(atoi(argv[1]));
+
+	if((lfd = socket(AF_INET, SOCK_STREAM, 0)) <0) { logger(ERROR, "Error during a syscall", "socket()"); }
+	if (bind(lfd, (struct sockaddr *)&server, sizeof(server)) < 0) { logger(ERROR, "Error during a syscall", "bind()"); }
+	if (listen(lfd, 64) < 0) { logger(ERROR, "Error during a syscall", "listen()"); }
+
+	for (;;) {
+		l = sizeof(cli);
+
+		if ((sock_fd = accept(lfd, (struct sockaddr *)&cli, &l)) < 0) { logger(ERROR, "Error during a syscall", "accept"); }
+		if ((pid = fork()) < 0) { logger(ERROR, "Error during a syscall", "fork()"); }
+
+		close(lfd);
+		if (pid == 0) {
+			request_handler(sock_fd);
+		}
+	}
+}
+
 int main(int argc, char **argv) {
-	// TODO
+	preliminary_checks(argc, argv);
+	spawn_server(argv);
 }
--- grocery.h
+++ grocery.h
@@ -1,2 +1,11 @@
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <netinet/in.h>
+#include "globals.h"
+#include "logger.h"
+#include "request_handler.h"
+
+void print_help();
+void preliminary_checks(int argc, char **argv);
+void spawn_server(char **argv);
