ef0ce1823fe582d2b60c02a1ff0352a337722ba3
Alessio Biancalana <dottorblaster@gmail.com>
2017-07-09 18:01:45 +0200

    huge work on the new conversion library to set up function

--- conversion.c
+++ conversion.c
@@ -1,16 +1,54 @@
 #include "conversion.h"
 
-int conversion_quality(char *str) {
-	printf(str);
-	printf("\n");
+int is_image_jpeg(char *tok) {
+    if (!strncmp(&tok[0], "image/jpeg", strlen("image/jpeg"))) {
+        return 1;
+    }
+    if (!strncmp(&tok[0], "image/jpg", strlen("image/jpg"))) {
+        return 1;
+    }
+    if (!strncmp(&tok[1], "image/jpeg", strlen("image/jpeg"))) {
+        return 1;
+    }
+    if (!strncmp(&tok[1], "image/jpg", strlen("image/jpg"))) {
+        return 1;
+    }
+    return 0;
+}
+
+int has_quality(char *tok) {
+    if (strstr(tok, ";q=") != NULL) {
+        return 1;
+    }
+    return 0;
+}
+
+int conversion_quality(char *header) {
+    char *tok, *str, *dbuf;
+    double quality;
+    int retval;
+
+    dbuf = str = strdup(header);
+    while ((tok = strsep(&str, ",")) != NULL) {
+        if (is_image_jpeg(tok) && has_quality(tok)) {
+            strsep(&tok, ";q=");
+            tok += 2;
+            quality = atof(tok)*100;
+            retval = (int) quality;
+            free(dbuf);
+            return retval;
+        }
+    }
+    free(dbuf);
 	return 0;
 }
 
 int cachehit(char *buf, hcontainer *headers) {
-	char *header, *pbuf;
+	char *header;
 	int q;
 	if (!strcmp(headers[0].val, "")) { return 0; }
-	q = conversion_quality(strdup(headers[0].val));
+
+	q = conversion_quality(headers[0].val);
 	
 	return 0;
 }
