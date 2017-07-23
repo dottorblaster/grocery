fe52ecd279036c9ea66a8365d430b2664b395429
Alessio Biancalana <dottorblaster@gmail.com>
2017-07-23 21:41:11 +0200

    finalize cache implementation, convert_img function WIP description
    convert_img implementation is still missing but it could be the missing piece for the shit

--- conversion.c
+++ conversion.c
@@ -51,7 +51,7 @@ char * cached_filename(char *buf, int quality) {
     str = strdup(buf);
 
     sprintf(quality_s, "%d", quality);
-    strcat(retval, str);
+    strcpy(retval, str);
     strcat(retval, "-");
     strcat(retval, quality_s);
 
@@ -59,32 +59,31 @@ char * cached_filename(char *buf, int quality) {
 }
 
 int cachehit(char *buf, hcontainer *headers) {
-	char *header, *cachedfn, *toaccess;
+	char *cachedfn, *toaccess;
 	int q;
 	if (!strcmp(headers[0].val, "")) { return 0; }
 
-	q = conversion_quality(headers[0].val);
-    // TODO
-    // if ((q = conversion_quality()) == 0) { return 0; }
+    if ((q = conversion_quality(headers[0].val)) == 0) { return 0; }
     cachedfn = cached_filename(buf, q);
 
     toaccess = malloc(sizeof(cachedfn)+8);
-    strcat(toaccess, "./cache/");
+    strcpy(toaccess, "./cache/");
 	strcat(toaccess, cachedfn);
 
     if (access(toaccess, F_OK | R_OK) == 0) {
         logger(LOG, "Cache hit!", "serving the cached file");
-        // TODO
-        // Serve the cached file
 	} else {
         logger(LOG, "Cache didn't hit", "still need to convert the file");
-        // TODO
-        // Convert the file, then serve
+        convert_img(buf, cachedfn, q);
     }
 
-	return 0;
+    strcpy(buf, cachedfn);
+	return 1;
 }
 
-int convert_img(char *str, int quality) {
+int convert_img(char *source, char *dest, int quality) {
     // TODO
+    // wrap the convert executable in an execp
+    // source, destination and quality
+    // are given as params
 }
