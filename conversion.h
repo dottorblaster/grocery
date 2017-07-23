ad0a554e10938beb75a627f0b00809b38b174fb9
Alessio Biancalana <dottorblaster@gmail.com>
2017-07-24 00:44:33 +0200

    Updated conversion header file to latest function sigs

--- conversion.h
+++ conversion.h
@@ -3,6 +3,10 @@
 #include "logger.h"
 #include "headers.h"
 
+int is_image_jpeg(char *tok);
+int has_quality(char *tok);
 int conversion_quality(char *str);
+char * cached_filename(char *buf, int quality);
 int cachehit(char *buf, hcontainer *headers);
-int convert_img(char *str, int quality);
+char * build_convert_cmd(char *source, char *dest, int quality);
+int convert_img(char *str, char *dest, int quality);
